2abe9c5faf67305b5efa8bc2c7d4fee35a866930
Gbemisola Victoria Ikuegbuwa <fadeyi.victoria@yahoo.com>
2026-01-02 16:14:55 +0100

    Add comments explaining total evaluation
    Added explanation about the evaluation of 'total' in JavaScript.

--- js1-wk1-eval/level-700.js
+++ js1-wk1-eval/level-700.js
@@ -4,3 +4,10 @@ console.log(total);
 
 // Without running the code, predict and explain what total will evaluate to
 // Use the JS1 Week 1 prep to help you work
+
+// it will evaluate to undefined.
+// In JavaScript, console.log() is used only for printing values to the console.
+// It does not return a meaningful value.
+
+// The return value of console.log() is undefined 
+// So total becomes undefined
