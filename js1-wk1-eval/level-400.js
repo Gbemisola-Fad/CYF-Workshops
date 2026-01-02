6b919e5a4f667c1e68f0211cbfa1bb156e572285
Gbemisola Victoria Ikuegbuwa <fadeyi.victoria@yahoo.com>
2026-01-02 15:50:10 +0100

    Document function calls and string operations in comments
    Added explanations and answers to questions about function calls and string manipulation.

--- js1-wk1-eval/level-400.js
+++ js1-wk1-eval/level-400.js
@@ -12,3 +12,22 @@ confession = confession.replaceAll(secret,'x'.repeat(secret.length));
 // c) How many inputs do we pass to replaceAll when we call it? How can you tell?
 // d) How any inputs do we pass to repeat when we call it?  How can you tell?
 // e) What kind of statement is on line 7?
+
+// a) There are two functions
+// confession.replaceALL(secret.length)
+// 'x'.repeat(secret.length)
+
+// b) The expression does 
+// secret holds the value "cake".
+// 'x'.repeat(secret.length): This part creates a string of 'x' characters, with a length equal to that of secret. the "cake" has a length of 4, 
+// this will produce "xxxx". confession.replaceAll(secret, 'x'.repeat(secret.length)) 
+// This replaces all occurrences of "cake" in the confession string with "xxxx"
+
+// c) The substring to be replaced (secret, which is "cake").
+// The replacement string ('x'.repeat(secret.length), which resolves to "xxxx").
+
+// d) We pass 1 input to repeat
+// There is only one value inside the parentheses
+// repeat is defined to take a single number indicating how many times to repeat the string
+
+// e) This is an assignment statement.
