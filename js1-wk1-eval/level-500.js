0eab4b9119d49e46364456ac58bb8aea85499ba8
Gbemisola Victoria Ikuegbuwa <fadeyi.victoria@yahoo.com>
2026-01-02 15:55:35 +0100

    Document avgDaysInMonth calculation logic
    Added explanation for avgDaysInMonth calculation.

--- js1-wk1-eval/level-500.js
+++ js1-wk1-eval/level-500.js
@@ -5,3 +5,9 @@ const avgDaysInMonth = Math.floor(365 / 12);
 // Without running any code,
 // Predict and explain what the value of avgDaysInMonth will evaluate to
 // You can use documentation to look up what Math.floor does
+
+// The expression Math.floor(365 / 12) calculates the average number of days in a month by dividing the total number of days in a year (365) by the number of months in a year (12).
+
+// 365/12 equals approximately 30.416666666666668.
+// The Math.floor() function in JavaScript rounds down any decimal number to the nearest whole number. So, when it processes 30.416666666666668, 
+// it will round it down to 30
