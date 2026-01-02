a26759c4d5bf8993b4e40a7cc9cb588b78a8496b
Gbemisola Victoria Ikuegbuwa <fadeyi.victoria@yahoo.com>
2026-01-02 16:07:26 +0100

    Add calculation for days left in the year

--- js1-wk1-eval/level-600.js
+++ js1-wk1-eval/level-600.js
@@ -6,3 +6,9 @@
 // Declare a new variable daysLeftInTheYear by forming an expression with dayNumberOfTheYear
 
 // Use console.log to check your answer
+
+//  Day number of the year from the website const dayNumberOfTheYear = 2;
+// Total days in the year (2026 is not a leap year) const totalDaysInYear = 365;
+// Calculate days left const daysLeftInTheYear = totalDaysInYear - dayNumberOfTheYear;
+// Log the result console.log("Day of the year:", dayNumberOfTheYear);
+// console.log("Days left in the year:", daysLeftInTheYear);
