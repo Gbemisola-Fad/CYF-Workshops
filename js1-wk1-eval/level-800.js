2ef81df45f1e8058cfb1f5cf3ab82b13cbfba22c
Gbemisola Victoria Ikuegbuwa <fadeyi.victoria@yahoo.com>
2026-01-03 12:37:11 +0100

    Change 'Facebook' to 'Meta' in comments
    Updated comments to reflect the new branding of Meta.

--- js1-wk1-eval/level-800.js
+++ js1-wk1-eval/level-800.js
@@ -7,4 +7,10 @@ and sharing more info on how our AI systems rank content.`
 const updatedPost = post.replaceAll('Facebook','Meta');
 
 // Use documentation to work out what updatedPost will evaluate to.
-// Don't run any code for this task
+// Don't run any code for this taskHow AI Influences What You See on Meta and Instagram
+
+
+// How AI Influences What You See on Meta and Instagram
+// We’re making it easier to control what you see on Meta and Instagram, 
+// and sharing more info on how our AI systems rank content.
+
